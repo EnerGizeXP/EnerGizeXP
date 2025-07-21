0c42c099c005a839451e0f6c48c083e7f9a9de2d
mrFavoslav <ochmel@favoslav.cz>
2025-07-21 04:48:59 +0200

    fix: fixed the xpPerCycle for higher tiers

--- src/main/java/mods/favoslav/energizexp/blocks/xpconv/BlockInfo.kt
+++ src/main/java/mods/favoslav/energizexp/blocks/xpconv/BlockInfo.kt
@@ -5,13 +5,14 @@ enum class BlockInfo(
     val maxXp: Int,
     val maxEnergy: Int,
     val energyPerCycle: Int,
+    val xpPerCycle: Int,
     val guild: Int
 ) {
-    BASIC("xpconv_basic", 400, 25000, 300, 0),
-    ADVANCED("xpconv_advanced", 800, 40000, 600, 1),
-    ELITE("xpconv_elite", 1600, 55000, 1200, 2);
+    BASIC("xpconv_basic", 400, 25000, 300, 2, 0),
+    ADVANCED("xpconv_advanced", 800, 40000, 600, 5, 1),
+    ELITE("xpconv_elite", 1600, 55000, 1200, 12, 2);
 
-    private val basexp = 2
+    /*private val basexp = 2
     val xpPerCycle: Int
-        get() = (basexp * (guild * 2) + guild).takeIf { it != 0 } ?: basexp
+        get() = (basexp * (guild * 2) + (guild * guild)).takeIf { it != 0 } ?: basexp*/
 }
